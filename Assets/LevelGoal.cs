9627def86046fb11ba2653478889ddae9a585fe4
David Majercak <davidmajercak@gmail.com>
2020-11-01 15:42:41 -0500

    Added help/tips menu.  Added 'You beat the demo' text

--- Assets/LevelGoal.cs
+++ Assets/LevelGoal.cs
@@ -8,6 +8,7 @@ public class LevelGoal : MonoBehaviour
 {
     TextMeshProUGUI _levelCompleteTMP;
     string _levelCompleteText;
+    [SerializeField] int _lastLevel;
     void OnTriggerEnter(Collider other) 
     {
         if (other.gameObject.tag == "Player")
@@ -32,7 +33,24 @@ public class LevelGoal : MonoBehaviour
         //Revert the timescale changes
         Time.timeScale = 1;
         Time.fixedDeltaTime = 0.02f;
-        //Load next scene
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        //If level is the last level
+        if(SceneManager.GetActiveScene().buildIndex == _lastLevel)
+        {
+            //Demo completion message
+            _levelCompleteTMP.text = "You beat the demo!\nStay tuned for more levels!";
+            yield return new WaitForSeconds(3f);
+            //Return CursorLockMode to normal
+            Cursor.lockState = CursorLockMode.None;
+            //Return to Main Menu
+            SceneManager.LoadScene(0);
+        }
+        //If level isn't the last level
+        else
+        {
+            //Load next scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+       
     }
 }
