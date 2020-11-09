7b5372b6a56b6f6a2a50525094857ced42ee54d6
David Majercak <davidmajercak@gmail.com>
2020-11-09 01:28:07 -0500

    Created collectable shader and initial script

---
+++ Assets/Collectable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    [SerializeField] float _rotationSpeed;
+    [SerializeField] float _frequency;
+    [SerializeField] float _magnitude;
+    Vector3 _originalPosition;
+
+    void Start()
+    {
+        _originalPosition = transform.position;
+    }
+    void Update()
+    {
+        transform.RotateAround(transform.position, new Vector3(0, 35, 45), _rotationSpeed * Time.deltaTime);
+        transform.position = _originalPosition + Vector3.up * Mathf.Sin(Time.time * _frequency) * _magnitude;
+    }
+}
