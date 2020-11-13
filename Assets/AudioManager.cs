8bd7e1fac34100ddc9a6b43c2a18ac0547292ba6
David Majercak <davidmajercak@gmail.com>
2020-11-13 00:06:00 -0500

    Working on playlist functionality, still does not work

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    AudioManager Instance;
+    AudioSource _audioSource;
+    [SerializeField] AudioClip[] _trackList;
+
+    void Awake()
+    {
+        Instance = this;
+        _audioSource = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        StartCoroutine(PlayNextSong());
+    }
+
+    IEnumerator PlayNextSong()
+    {
+        _audioSource.clip = _trackList[1];
+        yield return new WaitForSeconds(_trackList[1].length);
+        StartCoroutine(PlayNextSong());
+    }
+}
--- Assets/MoveObject.cs
+++ Assets/MoveObject.cs
@@ -11,21 +11,30 @@ public class MoveObject : MonoBehaviour
     [SerializeField] float _rotateYSpeed;
     [SerializeField] float _moveZSpeed;
     [SerializeField] float _rotateZSpeed;
-    Vector3 _initialPosition;
+    [SerializeField] float _sinYFrequency;
+    [SerializeField] float _sinYMagnitude;
+    Vector3 _basePosition;
     
     void Start()
     {
-        _initialPosition = gameObject.transform.position;
+        _basePosition = gameObject.transform.position;
     }
 
     void Update()
     {
+        //This doesn't allow for an object to move at the same time as sin wave
         if(_moveXSpeed != 0 || _moveYSpeed != 0 || _moveZSpeed != 0)
-            gameObject.transform.position = new Vector3(gameObject.transform.position.x + _moveXSpeed * Time.deltaTime, 
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x + _moveXSpeed * Time.deltaTime,
                 gameObject.transform.position.y + _moveYSpeed * Time.deltaTime,
                 gameObject.transform.position.z + _moveZSpeed * Time.deltaTime);
+        }
+            
 
         if(_rotateXSpeed != 0 || _rotateYSpeed != 0 || _rotateZSpeed != 0)
             gameObject.transform.Rotate(_rotateXSpeed * Time.deltaTime, _rotateYSpeed * Time.deltaTime, _rotateZSpeed * Time.deltaTime);
+
+        if(_sinYFrequency != 0 && _sinYMagnitude != 0)
+            transform.position = _basePosition + Vector3.up * Mathf.Sin(Time.time * _sinYFrequency) * _sinYMagnitude;
     }
 }
