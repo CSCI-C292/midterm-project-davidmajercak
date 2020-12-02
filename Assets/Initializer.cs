3b7a573d223aac1c9a4ee6f7fdde6d65475d3d53
David Majercak <davidmajercak@gmail.com>
2020-12-01 19:01:33 -0500

    Added Volume Slider to Settings menu and in PlayerPrefs

---
+++ Assets/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Initializer : MonoBehaviour
+{
+    [SerializeField] AudioMixer _audioMixer;
+    void Start()
+    {
+        _audioMixer.SetFloat("volume", Mathf.Log10(PlayerPrefs.GetFloat("volume", 0.25f)) * 20);
+    }
+}
---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField] AudioMixer _audioMixer;
+    [SerializeField] Slider _slider;
+
+    void Start()
+    {
+        _slider.value = PlayerPrefs.GetFloat("volume", 0.25f);
+    }
+    public void SetVolume(float volume)
+    {
+        _audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("volume", volume);
+    }
+}
